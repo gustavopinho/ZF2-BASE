0463bd358be50a2dc58621e277436136010196ac
Jorge Gustavo dos Santos Pinho <jorgegustavo@femc.edu.br>
2016-06-08 19:57:07 -0300

    [Acl] Criando Entitys e Forms

---
+++ module/Acl/src/Acl/Entity/Role.php
@@ -0,0 +1,226 @@
+<?php
+namespace Acl\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Zend\Stdlib\Hydrator;
+
+/**
+ * Class Role
+ *
+ * @ORM\Table(name=acl_role)
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Entity(repositoryClass="Acl\Repository\Role")
+ */
+class Role
+{
+    /**
+     * @var intenger
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=254, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\ManyToOne(targetEntity="Acl\Entity\Role")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="parent_id", referencedColumnName="id", nullable=true)
+     * })
+     */
+    private $parent;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="developer", type="boolean", nullable=false)
+     */
+    private $developer;
+
+    /**
+     * @var datetime
+     *
+     * @ORM\Column(name="created", type="datetime", nullable=false)
+     */
+    private $created;
+
+    /**
+     * @var datetime
+     *
+     * @ORM\Column(name="updated", type="datetime", nullable=false)
+     */
+    private $updated;
+
+    /**
+     * @param array $options
+     */
+    public function __construct($options=[])
+    {
+        (new Hydrator\ClassMethods())->hydrate($options, $this);
+        $this->setCreated()->setUpdated();
+    }
+
+    /**
+     * Get the value of Id
+     *
+     * @return intenger
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of Id
+     *
+     * @param intenger id
+     *
+     * @return self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of Name
+     *
+     * @param string name
+     *
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Parent
+     *
+     * @return integer
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Set the value of Parent
+     *
+     * @param integer parent
+     *
+     * @return self
+     */
+    public function setParent($parent)
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Developer
+     *
+     * @return boolean
+     */
+    public function getDeveloper()
+    {
+        return $this->developer;
+    }
+
+    /**
+     * Set the value of Developer
+     *
+     * @param boolean developer
+     *
+     * @return self
+     */
+    public function setDeveloper($developer)
+    {
+        $this->developer = $developer;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Created
+     *
+     * @return datetime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set the value of Created
+     *
+     * @return self
+     */
+    public function setCreated()
+    {
+        $this->created = = new \DateTime("now");
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Updated
+     *
+     * @return datetime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set the value of Updated
+     *
+     * @ORM\PrePersist
+     *
+     * @return self
+     */
+    public function setUpdated()
+    {
+        $this->updated = new \DateTime("now");
+
+        return $this;
+    }
+
+    /**
+     * Get the values of Role
+     * 
+     * @return array
+     */
+    public function toArray()
+    {
+        $array = (new Hydrator\ClassMethods())->extract($this);
+        $array['parent'] = $this->getParent() ? $this->getParent()->getId() : null;
+        return $array;
+    }
+}
